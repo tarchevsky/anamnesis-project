4d0dea4c7bb264ad74dafd7c85df5e4157fb69ca
tarchevskyivan <i.tarchevsky@gmail.com>
2023-11-28 17:24:05 +0300

    новый пароль приложения

--- mailer/smart.php
+++ mailer/smart.php
@@ -152,7 +152,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'marybelova-psy@yandex.ru';                 // Наш логин
-$mail->Password = 'pklliiewyjlwzrwt';                           // Наш пароль от ящика
+$mail->Password = 'jhrmpksblqhhdkpk';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
